132579150058dc43fbea951e8a1d2b8a7becd253
Aubaid <72610395+Aubaid12@users.noreply.github.com>
2024-10-23 23:56:38 +0530

    Update playwright.config.js
    mask token

--- playwright.config.js
+++ playwright.config.js
@@ -14,7 +14,7 @@ mode: 'testops',
 debug: false,
 testops: {
 api: {
-token: '96dc0541197af721a2d52a2b75846326937f1d708de8222322f43ed316d2652e',
+token: '96dc0541197af721a2d52a2b******************',
 },
 project: 'DEMO', // Replace <DEMO> with your project code
 uploadAttachments: true,
